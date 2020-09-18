99db753b9fe858114c0bbccda725230ee49f51c4
Alex Deckmyn <alex.deckmyn@meteo.be>
2020-09-18 16:15:07 +0200

    fix read_fatar()

--- R/read_fatar.R
+++ R/read_fatar.R
@@ -44,7 +44,7 @@ read_fatar <- function(file_name,
 
   # now we call read_fa for individual lead times and combine in a tibble
   ffun <- function(ldt, filelist) {
-    fcfile <- grep(sprintf("+%04i$", ldt), filelist, value = TRUE)
+    fcfile <- grep(sprintf("+%04i$", ldt), names(filelist), value = TRUE)
     if (length(fcfile) != 1) {
       stop("Lead time ", ldt, " not available in archive file\n",
            file_name, "\n", length(fcfile), " hits")
@@ -56,7 +56,7 @@ read_fatar <- function(file_name,
                                 ...)
   }
 
-  result <- purrr::map_dfr(lead_time, ffun, names(filelist))
+  result <- purrr::map_dfr(lead_time, ffun, filelist)
 #  result <- NULL
 #  for (ldt in seq_along(lead_time)) {
 #    fcfile <- grep(sprintf("+%04i$", lead_time[ldt]), names(filelist), value = TRUE)
