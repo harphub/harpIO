26d96fcce15ff68ef734f97e9882dd5ec6d256d6
Andrew Singleton <andrewts@met.no>
2023-11-17 10:55:34 +0100

    Get CRS from attributes if no proj string is available

--- R/get_domain_netcdf.R
+++ R/get_domain_netcdf.R
@@ -27,31 +27,32 @@ get_domain_netcdf <- function(file_name, opts) {
 
   if (is.null(proj4)) {
     if (proj4_var != 0 && !is.element(proj4_var, names(nc_id[["var"]]))) {
-      stop(
-        "Projection variable '", proj4_var, "' not found in netcdf file.\n",
-        "Use netcdf_opts() to set.",
-        call. = FALSE
-      )
+     stop(
+       "Projection variable '", proj4_var, "' not found in netcdf file.\n",
+       "Use netcdf_opts() to set.",
+       call. = FALSE
+     )
     }
-    if (is.null(proj4_att)) {
-      proj4 <- ncdf4::ncvar_get(proj4_var)
-      if (!grepl("+proj", proj4)) {
-        stop(
-          "Projection variable '", proj4_var, "' does not appear to be a proj string:\n",
-          proj4,
-          call. = FALSE
-        )
-      }
-    }
-    proj4 <- ncdf4::ncatt_get(nc_id, proj4_var, proj4_att)
-    if (!proj4[["hasatt"]]) {
-      stop(
-        "Projection variable '", proj4_var, "' does not have the attribute '", proj4_att, "'.\n",
-        "Use netcdf_opts() to set.",
-        call. = FALSE
-      )
-    }
-    proj4 <- proj4[["value"]]
+    # if (is.null(proj4_att)) {
+    #   proj4 <- ncdf4::ncvar_get(proj4_var)
+    #   if (!grepl("+proj", proj4)) {
+    #     stop(
+    #       "Projection variable '", proj4_var, "' does not appear to be a proj string:\n",
+    #       proj4,
+    #       call. = FALSE
+    #     )
+    #   }
+    # }
+    # proj4 <- ncdf4::ncatt_get(nc_id, proj4_var, proj4_att)
+    # if (!proj4[["hasatt"]]) {
+    #   stop(
+    #     "Projection variable '", proj4_var, "' does not have the attribute '", proj4_att, "'.\n",
+    #     "Use netcdf_opts() to set.",
+    #     call. = FALSE
+    #   )
+    # }
+    # proj4 <- proj4[["value"]]
+    proj4 <- get_crs_from_attrs(ncdf4::ncatt_get(nc_id, proj4_var))
   }
 
   if (proj4 == "wrf") {
@@ -193,3 +194,152 @@ get_domain_netcdf <- function(file_name, opts) {
   )
 
 }
+
+# Function is a copy of the .getCRSfromGridMap4 function from the terra package.
+# The only difference is that no test is done to check if the CRS is valid
+get_crs_from_attrs <- function(g) {
+
+  if (!is.null(g$epsg_code)) {
+    crs <- g$epsg_code
+    if (!grep("EPSG:", crs, ignore.case = TRUE)) {
+      crs <- paste0("epsg:", crs)
+    }
+    return(crs)
+  }
+
+  sp <- g$standard_parallel
+  if (length(sp) > 1) {
+    g$standard_parallel1 <- sp[1]
+    g$standard_parallel2 <- sp[2]
+    g$standard_parallel <- NULL
+  }
+
+  vals <- sapply(g, function(i) i[1])
+  vars <- names(vals)
+  if (any(vars %in% c("proj4", "crs_wkt", "spatial_ref"))) {
+    crs <- vals[vars %in% c("proj4", "crs_wkt", "spatial_ref")][1]
+    return(crs)
+  }
+  # based on info at
+  # http://trac.osgeo.org/gdal/wiki/NetCDF_ProjectionTestingStatus
+  # accessed 7 October 2012
+  prj <- matrix(
+    c(
+      "albers_conical_equal_area",
+      "aea",
+      "azimuthal_equidistant",
+      "aeqd",
+      "lambert_cylindrical_equal_area",
+      "cea",
+      "lambert_azimuthal_equal_area",
+      "laea",
+      "lambert_conformal_conic",
+      "lcc",
+      "latitude_longitude",
+      "longlat",
+      "mercator",
+      "merc",
+      "orthographic",
+      "ortho",
+      "polar_stereographic",
+      "stere",
+      "stereographic",
+      "stere",
+      "transverse_mercator",
+      "tmerc"
+    ),
+    ncol  = 2,
+    byrow = TRUE
+  )
+
+  m <- matrix(
+    c(
+      "grid_mapping_name",
+      "+proj",
+      "false_easting",
+      "+x_0",
+      "false_northing",
+      "+y_0",
+      "scale_factor_at_projection_origin",
+      "+k_0",
+      "scale_factor_at_central_meridian",
+      "+k_0",
+      "standard_parallel",
+      "+lat_1",
+      "standard_parallel1",
+      "+lat_1",
+      "standard_parallel2",
+      "+lat_2",
+      "longitude_of_central_meridian",
+      "+lon_0",
+      "longitude_of_projection_origin",
+      "+lon_0",
+      "latitude_of_projection_origin",
+      "+lat_0",
+      "straight_vertical_longitude_from_pole",
+      "+lon_0",
+      "longitude_of_prime_meridian",
+      "+pm",
+      "semi_major_axis",
+      "+a",
+      "semi_minor_axis",
+      "+b",
+      "inverse_flattening",
+      "+rf",
+      "earth_radius",
+      "+a"
+    ),
+    ncol = 2,
+    byrow = TRUE
+  )
+
+  # add logic that if prime merid is defined but not centr merid. centr merid is same as prime.
+
+  i <- match(vars, m[,1])
+  if (all(is.na(i))) {
+    gg <- cbind(vars, vals)
+    mtxt <- paste(
+      apply(gg, 1, function(x) paste(x, collapse = '=')),
+      collapse = '; '
+    )
+    warning("cannot process the crs\n", mtxt)
+    return(NA)
+  } else if (any(is.na(i))) {
+    vr <- vars[is.na(i)]
+    vl <- vals[is.na(i)]
+    gg <- cbind(vr, vl)
+    gg <- gg[!(gg[,1] %in% c("crs_wkt", "esri_pe_string")), ,drop = FALSE]
+    if (NROW(gg) > 0) {
+      mtxt <- paste(
+        apply(gg, 1, function(x) paste(x, collapse = '=')),
+        collapse = '\n'
+      )
+      warning("cannot process these parts of the crs:\n", mtxt)
+    }
+    vars <- vars[!is.na(i)]
+    vals <- vals[!is.na(i)]
+    i <- stats::na.omit(i)
+  }
+  tab <- cbind(m[i,], vals)
+  rr <- which(tab[,1] == "earth_radius")
+  if (length(rr) > 0) {
+    bb <- tab[rr,]
+    bb[2] <- "+b"
+    tab <- rbind(tab, bb)
+  }
+  p <- which(tab[,2] == '+proj')
+  if (length(p) == 0) {
+    warning("cannot create a valid crs\n", mtxt)
+    return(NA)
+  } else {
+    tab <- rbind(tab[p, ], tab[-p, ])
+  }
+  j <- match(tab[1,3], prj[,1])
+  tab[1,3] <- prj[j,2]
+
+  paste(
+    apply(tab[,2:3], 1, function(x) paste(x, collapse = '=')),
+    collapse = ' '
+  )
+}
+
