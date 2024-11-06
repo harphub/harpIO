8721422a658510749ef38fe55d3056425014c7af
Andrew Singleton <andrewts@met.no>
2024-11-06 08:55:06 +0100

    Add ISO 8601 week number to template for saving

--- R/save_point_verif.R
+++ R/save_point_verif.R
@@ -149,10 +149,12 @@ generate_verif_filename <- function(
   start_year  <- format(harpCore::as_dttm(start_date), "%Y")
   start_month <- format(harpCore::as_dttm(start_date), "%m")
   start_day   <- format(harpCore::as_dttm(start_date), "%d")
+  start_week  <- format(harpCore::as_dttm(start_date), "%V")
 
   end_year  <- format(harpCore::as_dttm(end_date), "%Y")
   end_month <- format(harpCore::as_dttm(end_date), "%m")
   end_day   <- format(harpCore::as_dttm(end_date), "%d")
+  end_week  <- format(harpCore::as_dttm(end_date), "%V")
 
   stringr::str_glue(template)
 
